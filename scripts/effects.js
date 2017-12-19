549b67f5c4a3b0c892d12489720dc019dd52e761
Steven Eisinger <steven.a.eisinger@gmail.com>
2017-12-19 00:00:11 -0500

    Fade in masthead

--- scripts/effects.js
+++ scripts/effects.js
@@ -2,13 +2,16 @@ var FADE_DURATION = 1000;
 
 $( document ).ready( function() {
     // Init page
-    $( '.page-content').hide();
-    $( '.page-content').css("visibility", "visible");
+    $( '.page-content' ).hide();
+    $( '.masthead' ).hide();
+    $( '.page-content' ).css( 'visibility', 'visible' );
+    $( '.masthead' ).css( 'visibility', 'visible' );
     $( '#home-page' ).fadeIn( FADE_DURATION );
+    $( '.masthead' ).fadeIn( FADE_DURATION );
 
     // Menu button handler
     $( '.nav-link' ).click( function() {
-        $( '.page-content').hide();
+        $( '.page-content' ).hide();
         $( '.nav-link' ).removeClass( 'active' );
         $( this ).addClass( 'active' );
         var button_href = $( this ).attr( 'href' );
