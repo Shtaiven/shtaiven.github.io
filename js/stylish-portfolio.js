d2daaa92957e22181b2d33bae79e3972bfee922b
Steven Eisinger <etienneshorty@gmail.com>
2018-06-05 03:27:53 -0400

    Consolidated files and added a dynamic modal for portfolio items!

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -42,23 +42,39 @@
 
 })(jQuery); // End of use strict
 
-// Disable Google Maps scrolling
-// See http://stackoverflow.com/a/25904582/1607849
-// Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
-  var that = $(this);
-  that.on('click', onMapClickHandler);
-  that.off('mouseleave', onMapMouseleaveHandler);
-  that.find('iframe').css("pointer-events", "none");
-}
-var onMapClickHandler = function(event) {
-  var that = $(this);
-  // Disable the click handler until the user leaves the map area
-  that.off('click', onMapClickHandler);
-  // Enable scrolling zoom
-  that.find('iframe').css("pointer-events", "auto");
-  // Handle the mouse leave event
-  that.on('mouseleave', onMapMouseleaveHandler);
-}
-// Enable map zooming with mouse scroll when the user clicks the map
-$('.map').on('click', onMapClickHandler);
+$(function() {
+  // Contact form
+  $("#submit").click(function(e) {
+    e.preventDefault();
+
+    var name = $("#name").val(),
+        email = $("#email").val(),
+        message = $("#message").val();
+
+    $.ajax({
+        type: "POST",
+        url: 'URL_ENDPOINT_FROM_LAST_STEP',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            'name': name,
+            'email': email,
+            'message': message
+        }),
+        success: function(res){
+            $('#form-response').text('Email was sent.');
+        },
+        error: function(){
+            $('#form-response').text('Error.');
+        }
+    });
+  })
+
+  // Portfolio modal
+  $(".portfolio-item").click(function() {
+    var title = $(this).find("h2").text();
+    var content = $(this).find("p").text();
+
+    $(".modal-title").text(title);
+    $(".modal-body").text(content);
+  })
+});
