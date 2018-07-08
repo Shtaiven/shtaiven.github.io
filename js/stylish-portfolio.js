0a1f3186b4137d41027dcab2de8d0337b6528a77
Steven Eisinger <etienneshorty@gmail.com>
2018-07-07 22:54:20 -0400

    Removed a word, used similar language throughout site, and corrected contact form JS

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -48,30 +48,4 @@
     $(".modal-title").text(title);
     $(".modal-body").text(content);
   })
-
-  // Contact form
-  $("#submit").click(function(e) {
-    e.preventDefault();
-
-    var name = $("#name").val(),
-        email = $("#email").val(),
-        message = $("#message").val();
-
-    $.ajax({
-        type: "POST",
-        url: 'URL_ENDPOINT_FROM_LAST_STEP',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            'name': name,
-            'email': email,
-            'message': message
-        }),
-        success: function(res){
-            $('#form-response').text('Email was sent.');
-        },
-        error: function(){
-            $('#form-response').text('Error.');
-        }
-    });
-  })
 })(jQuery); // End of use strict
